478c8ea5ca3abdf69752d14210c87b37007ae170
arulmurugan2001 <arul99811@gmail.com>
2026-07-24 13:03:38 +0530

    Remove temporary backend files from repo

--- tmp/show_mo_columns.js
+++
@@ -1,6 +0,0 @@
-const { executeQuery } = require('../src/config/database');
-
-executeQuery('SHOW COLUMNS FROM merchandise_orders')
-  .then(c => console.log(c.map(r => r.Field).join(', ')))
-  .catch(e => console.error(e))
-  .finally(() => process.exit(0));

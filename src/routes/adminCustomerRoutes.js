624f0082c1aa9a8781f504c59181f840b5ba57be
arulmurugan2001 <arul99811@gmail.com>
2026-08-12 15:04:59 +0530

    Harden admin RBAC API security

--- src/routes/adminCustomerRoutes.js
+++ src/routes/adminCustomerRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/adminCustomerController');
 const { verifyAdminToken, requirePermission } = require('../middleware/authMiddleware');
-// TODO: protect with admin auth middleware when available
-router.get('/', ctrl.getAll);
-router.get('/:id', ctrl.getOne);
-router.get('/:id/login-logs', ctrl.getLogs);
+router.get('/', verifyAdminToken, requirePermission('customers', 'view'), ctrl.getAll);
+router.get('/:id', verifyAdminToken, requirePermission('customers', 'view'), ctrl.getOne);
+router.get('/:id/login-logs', verifyAdminToken, requirePermission('customers', 'view'), ctrl.getLogs);
 router.put('/:id/status', verifyAdminToken, requirePermission('customers', 'edit'), ctrl.updateStatus);
 router.delete('/:id', verifyAdminToken, requirePermission('customers', 'delete'), ctrl.deleteCustomer);
 module.exports = router;
--- src/routes/adminPageHeroRoutes.js
+++ src/routes/adminPageHeroRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getAll, getByPageKeyAdmin, updateByPageKey } = require('../controllers/pageHeroController');
-const { verifyAdminToken } = require('../middleware/authMiddleware');
+const { verifyAdminToken, requirePermission } = require('../middleware/authMiddleware');
 const { pageHeroUpload } = require('../config/multer');
 
-router.get('/', verifyAdminToken, getAll);
-router.get('/:pageKey', verifyAdminToken, getByPageKeyAdmin);
-router.put('/:pageKey', verifyAdminToken, pageHeroUpload.fields([{ name: 'hero_image', maxCount: 1 }, { name: 'mobile_hero_image', maxCount: 1 }]), updateByPageKey);
+router.get('/', verifyAdminToken, requirePermission('site_settings', 'view'), getAll);
+router.get('/:pageKey', verifyAdminToken, requirePermission('site_settings', 'view'), getByPageKeyAdmin);
+router.put('/:pageKey', verifyAdminToken, requirePermission('site_settings', 'edit'), pageHeroUpload.fields([{ name: 'hero_image', maxCount: 1 }, { name: 'mobile_hero_image', maxCount: 1 }]), updateByPageKey);
 
 module.exports = router;
